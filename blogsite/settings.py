2e07992943f70b1ee69535d9c71083b856a6d612
Prince <pmasuper34@gmail.com>
2021-04-30 15:56:48 +0500

    changed database

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -84,10 +84,8 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER' : 'blog',
-        'PASSWORD':'Persiantigerboss45'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- blogsite/urls.py
+++ blogsite/urls.py
@@ -22,6 +22,6 @@ sitemaps = {'posts':PostSitemap,}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps':sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
